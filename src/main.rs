71338f9d9d5fa47728b4020145f09e121a144430
Ndagi Stanley <ndagis@gmail.com>
2021-03-19 10:38:39 +0300

    Add functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to the Rust API"
+}
+#[get("/greet/<name>")]
+fn greet(name: String) -> String {
+    format!("Hey {}, glad to have you here!", name)
+}
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index,greet]).launch();
 }
